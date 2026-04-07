1106ad7a20a673ef8d4b408980ea1801a53f2771
W1nd0wr <window.gitio@protonmail.com>
2026-04-07 18:46:11 +0100

    Initialize Supabase client in auth.js

---
+++ auth.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://tughhrextvsmnxhvyrts.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1Z2hocmV4dHZzbW54aHZ5cnRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMDY3NzUsImV4cCI6MjA4ODU4Mjc3NX0.R8lwINUXp2IuExj_wLakbLESMVSpzGXLDuLd_R3cH94"
+);
